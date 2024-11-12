bc643cc56ccb4060329db170be2956f93ab4d1f0
danielcreis <daniel.cvt7@gmail.com>
2024-11-12 19:39:46 -0300

    Remove error in API

--- API/index.js
+++ API/index.js
@@ -1,21 +1,26 @@
 const express = require("express");
-const axios = require("axios"); 
+const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Bem-vindo à API de Repositórios!");
+});
+
 app.get("/repos", async (req, res) => {
   try {
-
     const response = await axios.get(
       "https://api.github.com/users/takenet/repos?sort=created&direction=asc"
     );
 
-
-    const repos = response.data.slice(0, 5).map((repo) => ({
-      title: repo.full_name,
-      description: repo.description,
-      image: "https://avatars.githubusercontent.com/u/21820756?v=4",
-    }));
+    const repos = response.data
+      .filter((repo) => repo.language === "C#")
+      .slice(0, 5)
+      .map((repo) => ({
+        title: repo.full_name,
+        subtitle: repo.description || "Sem descrição",
+        image: "https://avatars.githubusercontent.com/u/21820756?v=4",
+      }));
 
     res.json(repos);
   } catch (error) {
